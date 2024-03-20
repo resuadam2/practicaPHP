7eda4e4f34608508f208272a876732c475a0aad3
resuadam2 <resuadam2@gmail.com>
2024-03-20 20:35:08 +0100

    20-3-24

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- footer.php -->
+<div class="container">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <span class="mb-3 mb-md-0 text-body-secondary">Proyecto de práctica @resuadam2</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-body-secondary" href="https://github.com/resuadam2/practicaPHP"><i class="bi bi-github"></i></a></li>
+      <!--
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+-->
+    </ul>
+</footer>
+</div>
